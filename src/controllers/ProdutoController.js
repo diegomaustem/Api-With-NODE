4fda50caad3588c199e9ff8e284473f66c429327
diegomaustem <diego.maustem@gmail.com>
2022-01-04 14:30:08 -0300

    conexao-mysql-ok

---
+++ src/controllers/ProdutoController.js
@@ -0,0 +1,5 @@
+const ProdutoService = require('../services/ProdutoService')
+
+
+
+
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express') 
+const router = express.Router()
+
+const ProdutoController = require('./controllers/ProdutoController')
+
+module.exports = router;
+
+
